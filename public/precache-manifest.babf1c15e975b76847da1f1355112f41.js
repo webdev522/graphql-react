f8b4330a19624e735372a9e58ef2293621fab9cf
webdev522 <webdev522@gmail.com>
2019-02-22 03:04:27 +0800

    complete

---
+++ public/precache-manifest.babf1c15e975b76847da1f1355112f41.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ab10d000cdf7ee45a57395feee160ad2",
+    "url": "/static/media/logo.ab10d000.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8f426090a738ed91b49a",
+    "url": "/static/js/main.8f426090.chunk.js"
+  },
+  {
+    "revision": "2201fe3db27479e6bd5d",
+    "url": "/static/js/1.2201fe3d.chunk.js"
+  },
+  {
+    "revision": "8f426090a738ed91b49a",
+    "url": "/static/css/main.f2fad161.chunk.css"
+  },
+  {
+    "revision": "4d874c8ac38b08cc580e3f11a0509abb",
+    "url": "/index.html"
+  }
+];
